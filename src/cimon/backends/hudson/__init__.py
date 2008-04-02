85f330942c03dfa83274e546cc88b05112f97db9
brian <brian@19d8b53b-2c48-0410-af07-93e3b2b4aec9>
2008-04-02 22:22:34 +0000

    refactors
    git-svn-id: file:///Users/bmunroe/svnrepo/gcimon/trunk@24 19d8b53b-2c48-0410-af07-93e3b2b4aec9

--- src/cimon/backends/hudson/__init__.py
+++
@@ -1,9 +0,0 @@
-PLUGIN_NAME = 'Hudson CI Server Backend'
-
-class Backend(object):
-	
-	def __init__(self):
-		pass
-	
-	def getProjectStatus(self,projectName=None):
-		return "400_OK"
--- src/cimon/main.py
+++
@@ -1,6 +0,0 @@
-import backends
-
-for x in backends.getAvailableBackends():
-	print x.PLUGIN_NAME
-	be = x.Backend()
-	print be.getProjectStatus()
