a0dfe28adf42f4e0430af766aa242d29e804311b
brian <brian@19d8b53b-2c48-0410-af07-93e3b2b4aec9>
2008-04-02 22:21:50 +0000

    dailies
    git-svn-id: file:///Users/bmunroe/svnrepo/gcimon/trunk@23 19d8b53b-2c48-0410-af07-93e3b2b4aec9

--- src/tests/runner.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class HelloTest(unittest.TestCase):
-	def testForHelloWorldness(self):
-		"""TestForHelloWorldness verifies that main.helloWorld() generates Hello, World"""
-		self.assertEquals("Hello, World","Hello, World")
-		
-if __name__ == "__main__":
-	unittest.main()
