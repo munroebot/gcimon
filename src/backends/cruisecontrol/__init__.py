46bece19f48481a21f6182f27d89a0082bdf6419
brian <brian@19d8b53b-2c48-0410-af07-93e3b2b4aec9>
2008-04-03 15:49:08 +0000

    dailes
    git-svn-id: file:///Users/bmunroe/svnrepo/gcimon/trunk@33 19d8b53b-2c48-0410-af07-93e3b2b4aec9

--- src/backends/cruisecontrol/__init__.py
+++ src/backends/cruisecontrol/__init__.py
@@ -1,6 +1,9 @@
 PLUGIN_NAME = 'CruiseControl'
+PLUGIN_STAT = 'MOCK'
 
 class Backend(object):
-    def __init__(self,projectName=None):
-	pass
+	def __init__(self):
+		pass
 
+	def getProjectStatus(self,projectName=None):
+		return "400_OK"
