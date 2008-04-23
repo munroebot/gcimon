e491083da98a78e17e985facdc720aff75e2a47c
brian <brian@19d8b53b-2c48-0410-af07-93e3b2b4aec9>
2008-04-23 16:07:52 +0000

    dailies
    git-svn-id: file:///Users/bmunroe/svnrepo/gcimon/trunk@46 19d8b53b-2c48-0410-af07-93e3b2b4aec9

--- src/gcimon/types/__init__.py
+++ src/gcimon/types/__init__.py
@@ -1,3 +1,21 @@
+# gcimon/__init__.py
+# Copyright 2008, Brian Munroe <brian.e.munroe@gmail.com>
+
+# This file is part of gcimon.
+#
+# gcimon is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# gcimon is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with gcimon.  If not, see <http://www.gnu.org/licenses/>.
+
 class Project(object):
     BUILD_STATUS_OK = "400_OK"
     BUILD_STATUS_BROKE = "200_BROKE"
