e429b2f78e592ad1ed0a6c4c71eb4c524949761c
brian <brian@19d8b53b-2c48-0410-af07-93e3b2b4aec9>
2008-04-23 02:07:37 +0000

    dailies
    git-svn-id: file:///Users/bmunroe/svnrepo/gcimon/trunk@40 19d8b53b-2c48-0410-af07-93e3b2b4aec9

--- tests/cimon_tests.py
+++ tests/cimon_tests.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+import xmlrunner
 
 sys.path.append('../src') # needed for loading backends
 
@@ -10,29 +11,27 @@ class BackendModuleWellFormedness(unittest.TestCase):
     def test_BACKEND_NAME(self):
         """ All backends should have a BACKEND_NAME"""
         for x in backends.getAvailableBackends():
-            try:
-                self.assertTrue(x.BACKEND_NAME != None)
-            except:
-                print x
-                raise AssertionError
-
+            self.assertTrue(x.BACKEND_NAME != None)
+    
     def test_BACKEND_DESCRIPTION(self):
         """ All backends should have a BACKEND_DESCRIPTION"""
         for x in backends.getAvailableBackends():
-            try:
-                self.assertTrue(x.BACKEND_DESCRIPTION != None)
-            except:
-                print x
-                raise AssertionError
+            self.assertTrue(x.BACKEND_DESCRIPTION != None)
 
     def test_BACKEND_VERSION(self):
         """ All backends should have a BACKEND_VERSION"""
         for x in backends.getAvailableBackends():
-            try:
-                self.assertTrue(x.BACKEND_VERSION != None)
-            except:
-                print x
-                raise AssertionError
+            self.assertTrue(x.BACKEND_VERSION != None)
+
+suite = unittest.TestSuite()
+suite.addTest(BackendModuleWellFormedness("test_BACKEND_NAME"))
+suite.addTest(BackendModuleWellFormedness("test_BACKEND_DESCRIPTION"))
+suite.addTest(BackendModuleWellFormedness("test_BACKEND_VERSION"))
 
 if __name__ == "__main__":
-    unittest.main()
+    
+    #runner = unittest.TextTestRunner()
+    runner = xmlrunner.XmlTestRunner(sys.stdout)
+    runner.run(suite)
+
+    #unittest.main()
